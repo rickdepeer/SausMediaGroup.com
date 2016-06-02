c06c48c4c428964843a9d57d5b115f838afb4823
rickdepeer <rickdepeer@gmail.com>
2016-06-02 16:23:52 +0200

    Revert "Title has changed"
    This reverts commit a8ed4fa594917bcaae037b9a963a91c11af08aac.

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,7 +4,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Title has changed</title>
+    <title>Home</title>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <!-- Latest compiled and minified CSS -->
