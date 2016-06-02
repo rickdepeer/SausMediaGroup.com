8ddd266e5a9f81ec333ee70d7422a5cbb0bb6a06
rickdepeer <rickdepeer@gmail.com>
2016-06-02 16:27:27 +0200

    Final Title

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,7 +4,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Home</title>
+    <title>Title</title>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <!-- Latest compiled and minified CSS -->
